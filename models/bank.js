db35241885c896dddcd8ea83078d712cc1eccda0
Ayantaker <saha.ayan1996@gmail.com>
2020-03-28 09:46:40 +0530

    Adding all files of rentweb

---
+++ models/bank.js
@@ -0,0 +1,18 @@
+var mongoose=                require("mongoose")
+var bcrypt = require('bcrypt');
+
+   
+
+var bankschema=new mongoose.Schema({
+    username:String,
+    name:String,
+    account_no:String,
+    ifsc:String,
+    
+    
+});
+bankschema.methods.generateHash= function(account_no){
+    return bcrypt.hashSync(account_no,bcrypt.genSaltSync(9));
+}
+
+module.exports=mongoose.model("bank",bankschema);
